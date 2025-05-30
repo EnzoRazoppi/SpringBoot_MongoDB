d6086c328d2ed2f1c2cb06c3e0fbb9c76826ac99
Razoppi <razoppienzo@gmail.com>
2025-05-30 11:44:21 -0300

    First commit/ Project created

---
+++ src/main/java/com/EnzoRazoppi/SpringBoot_MongoDB/SpringBootMongoDb1Application.java
@@ -0,0 +1,13 @@
+package com.EnzoRazoppi.SpringBoot_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoDb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/EnzoRazoppi/SpringBoot_MongoDB/SpringBootMongoDb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EnzoRazoppi.SpringBoot_MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
