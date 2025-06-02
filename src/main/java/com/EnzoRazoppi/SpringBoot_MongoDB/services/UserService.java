26cc31700533e24f74f9bcb3255816fa3672eef2
Razoppi <razoppienzo@gmail.com>
2025-06-02 18:58:23 -0300

    Get users by id

--- src/main/java/com/EnzoRazoppi/SpringBoot_MongoDB/services/UserService.java
+++ src/main/java/com/EnzoRazoppi/SpringBoot_MongoDB/services/UserService.java
@@ -1,12 +1,14 @@
 package com.EnzoRazoppi.SpringBoot_MongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.EnzoRazoppi.SpringBoot_MongoDB.domain.User;
 import com.EnzoRazoppi.SpringBoot_MongoDB.repository.UserRepository;
+import com.EnzoRazoppi.SpringBoot_MongoDB.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
