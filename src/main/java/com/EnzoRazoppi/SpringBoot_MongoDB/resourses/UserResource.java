c6df0690c822ee46822af04884ea215ef0204a08
Razoppi <razoppienzo@gmail.com>
2025-06-03 10:30:39 -0300

    insert through post method

--- src/main/java/com/EnzoRazoppi/SpringBoot_MongoDB/resourses/UserResource.java
+++ src/main/java/com/EnzoRazoppi/SpringBoot_MongoDB/resourses/UserResource.java
@@ -1,5 +1,6 @@
 package com.EnzoRazoppi.SpringBoot_MongoDB.resourses;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.EnzoRazoppi.SpringBoot_MongoDB.domain.User;
 import com.EnzoRazoppi.SpringBoot_MongoDB.dto.UserDTO;
@@ -35,4 +39,13 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); //cria o caminho(localização) de onde foi inserido para retornar no cabeçalho 
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
