4cbd55cffd02169fc7b92c547df61aafb22d853e
Razoppi <razoppienzo@gmail.com>
2025-06-10 17:19:00 -0300

    Creating queries

--- src/main/java/com/EnzoRazoppi/SpringBoot_MongoDB/repository/PostRepository.java
+++ src/main/java/com/EnzoRazoppi/SpringBoot_MongoDB/repository/PostRepository.java
@@ -1,11 +1,18 @@
 package com.EnzoRazoppi.SpringBoot_MongoDB.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.EnzoRazoppi.SpringBoot_MongoDB.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")  //consulta do MongoDB em forma de JSON
+	List<Post> searchTitle(String text);
 
+	List<Post> findByTitleContainingIgnoreCase(String text); // pelo nome do método o Spring Data consegue gerar a consulta
 }
